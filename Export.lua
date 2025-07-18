3cb89978a2dc0cc108425469cfbc6b8da04670fc
Bisolino <153382717+Bisolino@users.noreply.github.com>
2025-07-18 13:06:52 -0300

    Update Export.lua

--- Export.lua
+++ Export.lua
@@ -8,49 +8,46 @@ function GW.Export.ShowSummary()
     
     print("|cFFFFFF00" .. GW.L.GetString("SUMMARY_TITLE") .. "|r")
     
-    if GW.DB.isTracking then
-        local g, s, c = GW.DB.earnings[1] or 0, GW.DB.earnings[2] or 0, GW.DB.earnings[3] or 0
+    if GW.IsTracking() then
+        -- Formatar ganhos usando o novo sistema unificado
+        local earnings = GW.Util.FormatMoneyTable(GW.DB.earnings, "colored")
         local duration = time() - GW.DB.sessionStart
+        local gph = GW.Data.GetHourlyRate()
         local currentZone = GW.Data.GetCurrentZone()
         
         print("|cFF00FF00" .. GW.L.GetString("CURRENT_SESSION") .. ":|r")
-        print(string.format("|cFFFFD700• %s:|r %d |cFFC0C0C0%s:|r %d |cFFCC9900%s:|r %d", 
-            GW.L.GetString("GOLD_LABEL"), g, GW.L.GetString("SILVER_LABEL"), s, GW.L.GetString("COPPER_LABEL"), c))
-        print(string.format("|cFFFFD700• %s:|r %s", GW.L.GetString("DURATION_LABEL"), GW.Util.FormatTime(duration)))
-        print(string.format("|cFFFFD700• %s:|r %s", GW.L.GetString("LOCATION_LABEL"), currentZone))
-        
-        local hourlyGold, hourlySilver, hourlyCopper = GW.Data.GetHourlyRate()
+        print("|cFFFFD700• " .. GW.L.GetString("EARNINGS_LABEL") .. ":|r " .. earnings)
+        print("|cFFFFD700• " .. GW.L.GetString("DURATION_LABEL") .. ":|r " .. GW.Util.FormatTime(duration))
+        print("|cFFFFD700• " .. GW.L.GetString("LOCATION_LABEL") .. ":|r " .. currentZone)
         print("|cFF00FF00" .. GW.L.GetString("GPH_LABEL") .. " (60min):|r")
-        print(string.format("|cFFFFD700• %s:|r %d |cFFC0C0C0%s:|r %d |cFFCC9900%s:|r %d", 
-            GW.L.GetString("GOLD_LABEL"), hourlyGold, GW.L.GetString("SILVER_LABEL"), hourlySilver, GW.L.GetString("COPPER_LABEL"), hourlyCopper))
+        print("|cFFFFD700• " .. GW.L.GetString("PROJECTION") .. ":|r " .. gph)
+        
+        -- Mostrar alertas hyperspawn ativos
+        if GW.DB.hyperspawnAlerts and #GW.DB.hyperspawnAlerts > 0 then
+            print("|cFFFF0000===== " .. GW.L.GetString("HYPERSPAWN_ALERTS") .. " =====|r")
+            for i, alert in ipairs(GW.DB.hyperspawnAlerts) do
+                local timeStr = GW.Util.FormatTime(alert.time - GW.DB.sessionStart)
+                print(string.format("|cFFFFD700• [%s] |r%.0f%% %s", 
+                    timeStr, 
+                    alert.percentage,
+                    GW.L.GetString("ABOVE_AVERAGE")))
+            end
+        end
     elseif GW.DB.lastSession then
-        local earnings = GW.DB.lastSession.earnings
-        local g, s, c = earnings[1], earnings[2], earnings[3]
-        local duration = GW.DB.lastSession.elapsed
-        local gph = GW.DB.lastSession.gph or {0,0,0}
-        local hourlyGold, hourlySilver, hourlyCopper = gph[1], gph[2], gph[3]
+        -- Formatar dados da última sessão
+        local earnings = GW.Util.FormatMoneyTable(GW.DB.lastSession.earnings, "colored")
+        local duration = GW.Util.FormatTime(GW.DB.lastSession.elapsed)
+        local gph = GW.Util.FormatMoneyTable(GW.DB.lastSession.gph, "colored")
+        local location = GW.DB.lastSession.mainZone or GW.L.GetString("UNKNOWN_ZONE")
         
         print("|cFF00FF00" .. GW.L.GetString("LAST_SESSION") .. ":|r")
-        print(string.format("|cFFFFD700• %s:|r %d |cFFC0C0C0%s:|r %d |cFFCC9900%s:|r %d", 
-            GW.L.GetString("GOLD_LABEL"), g, GW.L.GetString("SILVER_LABEL"), s, GW.L.GetString("COPPER_LABEL"), c))
-        print(string.format("|cFFFFD700• %s:|r %s", GW.L.GetString("DURATION_LABEL"), GW.Util.FormatTime(duration)))
+        print("|cFFFFD700• " .. GW.L.GetString("EARNINGS_LABEL") .. ":|r " .. earnings)
+        print("|cFFFFD700• " .. GW.L.GetString("DURATION_LABEL") .. ":|r " .. duration)
         print("|cFF00FF00" .. GW.L.GetString("GPH_LABEL") .. ":|r")
-        print(string.format("|cFFFFD700• %s:|r %d |cFFC0C0C0%s:|r %d |cFFCC9900%s:|r %d", 
-            GW.L.GetString("GOLD_LABEL"), hourlyGold, GW.L.GetString("SILVER_LABEL"), hourlySilver, GW.L.GetString("COPPER_LABEL"), hourlyCopper))
+        print("|cFFFFD700• " .. GW.L.GetString("GPH_LABEL") .. ":|r " .. gph)
+        print("|cFFFFD700• " .. GW.L.GetString("LOCATION_LABEL") .. ":|r " .. location)
     else
         print("|cFFFF0000" .. GW.L.GetString("NO_DATA") .. "|r")
-    end
-    
-    -- Adicionar alertas de hyperspawn
-    if GW.DB.hyperspawnAlerts and #GW.DB.hyperspawnAlerts > 0 then
-        print("|cFFFF0000===== " .. GW.L.GetString("HYPERSPAWN_ALERTS") .. " =====|r")
-        for i, alert in ipairs(GW.DB.hyperspawnAlerts) do
-            local timeStr = GW.Util.FormatTime(alert.time - (GW.DB.sessionStart or alert.time))
-            print(string.format("|cFFFFD700• [%s] |r%.0f%% %s", 
-                timeStr, 
-                alert.percentage,
-                GW.L.GetString("ABOVE_AVERAGE")))
-        end
     end
     
     print("|cFFFFFF00================================|r")
@@ -68,19 +65,19 @@ function GW.Export.ShowFullSummary()
         local startTime = session.startTime or time()
         local dateStr = date("%d/%m %H:%M", startTime)
         local durationStr = GW.Util.FormatTime(session.elapsed)
-        local earnings = session.earnings or {0, 0, 0}
-        local g = earnings[1] or 0
-        local s = earnings[2] or 0
-        local c = earnings[3] or 0
-        local gph = session.gph or {0, 0, 0}
-        local gph_g = gph[1] or 0
-        local gph_s = gph[2] or 0
-        local gph_c = gph[3] or 0
+        local earnings = GW.Util.FormatMoneyTable(session.earnings, "colored")
+        local gph = GW.Util.FormatMoneyTable(session.gph, "colored")
         local location = session.mainZone or GW.L.GetString("UNKNOWN_ZONE")
         
         print(string.format("|cFF00FF00[%s]|r %s - %s", dateStr, location, durationStr))
-        print(string.format("|cFFFFD700• %s:|r %dg %ds %dc", GW.L.GetString("EARNINGS_LABEL"), g, s, c))
-        print(string.format("|cFFFFD700• %s:|r %dg %ds %dc", GW.L.GetString("GPH_LABEL"), gph_g, gph_s, gph_c))
+        print("|cFFFFD700• " .. GW.L.GetString("EARNINGS_LABEL") .. ":|r " .. earnings)
+        print("|cFFFFD700• " .. GW.L.GetString("GPH_LABEL") .. ":|r " .. gph)
+        
+        -- Mostrar se houve alerta hyperspawn nesta sessão
+        if session.hyperspawnAlerts and #session.hyperspawnAlerts > 0 then
+            print("|cFFFF0000• " .. GW.L.GetString("HYPERSPAWN_ALERTS") .. ":|r " .. #session.hyperspawnAlerts)
+        end
+        
         print("----------------------------------------")
     end
     
